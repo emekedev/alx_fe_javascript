c58408a24454418be76333cbc7e7ff2cfbeab141
Nwannadi Emeke <emekedev@gmail.com>
2025-06-28 16:13:22 +0100

    Added JS file

---
+++ dom-manipulation/script.js
@@ -0,0 +1,9 @@
+let newQuoteText = document.getElementById('newQuoteText');
+let newQuoteCategory = document.getElementById('newQuoteCategory');
+
+const quotesArray = [
+    {
+        text: newQuoteText,
+        category: newQuoteCategory
+    }
+]
